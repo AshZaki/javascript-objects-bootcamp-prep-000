9ef65c3f8288140da0a434577832e4c36f079039
Ashley  Zaki <ahnahn.un@gmail.com>
2018-08-23 17:18:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,11 @@
 
 
 function updatePlaylist(playlist,artistName,songTitle){
+<<<<<<< HEAD
    console.log(playlist[artistName]= songTitle);
+=======
+   console.log('playlist[artistName]= songTitle');
+>>>>>>> 3d8dd253892e724df75691efbc8b38f7f1f2d05b
    return playlist;
 }
 
